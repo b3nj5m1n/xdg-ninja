d29faf50ff1c3c7be5b7a63c4fc3d78c9580f795
Layerex <layerex@dismail.de>
2022-05-23 15:21:02 +0300

    Call jq only once

--- xdg-ninja.sh
+++ xdg-ninja.sh
@@ -174,29 +174,24 @@ decode_string() {
 /g' -e 's/\\\"/\"/g')" # Replace \n with literal newline and \" with "
 }
 
-# Reads a file from programs/, calls check_file on each file specified for the program
-check_program() {
-    PROGRAM=$1
-
+# Reads files from programs/, calls check_file on each file specified for each program
+do_check_programs() {
     while IFS="
 " read -r name; read -r filename; read -r movable; read -r help; do
         check_file "$(decode_string "$name")" "$(decode_string "$filename")" "$movable" "$(decode_string "$help")"
     done <<EOF
-$(jq '.files[] as $files | .name, $files.path, $files.movable, $files.help' "$PROGRAM")
+$(jq 'inputs as $input | $input.files[] as $file | $input.name, $file.path, $file.movable, $file.help' "$(dirname "$0")"/programs/*)
 EOF
 }
 
-# Loops over all files in the programs/ directory and calls check_program
-enumerate_programs() {
+check_programs() {
     printf "\033[1;3mStarting to check your \033[1;36m\$HOME.\033[1;0m\n"
     printf "\n"
-    for prog_filename in "$(dirname "$0")"/programs/*; do
-        check_program "$prog_filename"
-    done
+    do_check_programs
     printf "\033[1;3mDone checking your \033[1;36m\$HOME.\033[1;0m\n"
     printf "\n"
     printf "\033[3mIf you have files in your \033[1;36m\$HOME\033[1;0m that shouldn't be there, but weren't recognised by xdg-ninja, please consider creating a configuration file for it and opening a pull request on github.\033[1;0m\n"
     printf "\n"
 }
 
-enumerate_programs
+check_programs
