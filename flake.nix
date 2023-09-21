29895639877ba6c82f3ccec720c30cb741714bb8
Sandro Jäckel <sandro.jaeckel@gmail.com>
2023-09-22 00:54:54 +0200

    Misc cleanup

--- flake.nix
+++ flake.nix
@@ -10,10 +10,6 @@
   }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        runtimeDependencies = with pkgs; [
-          glow
-          jq
-        ];
         overlays = [
           (self: super: {
             xdg-ninja = super.xdg-ninja.overrideAttrs (old: {
@@ -26,7 +22,7 @@
       in rec {
         packages = flake-utils.lib.flattenTree {
           # The shell script and configurations, uses derivation from offical nixpkgs
-          xdg-ninja = pkgs.stdenv.mkDerivation rec {
+          xdg-ninja = pkgs.stdenv.mkDerivation {
             pname = "xdg-ninja";
             version = "0.1.0";
 
@@ -40,7 +36,7 @@
               DESTDIR="$out" PREFIX="/usr" make install
 
               wrapProgram "$out/usr/bin/xdg-ninja" \
-                --prefix PATH : "${pkgs.lib.makeBinPath [ pkgs.glow pkgs.jq ]}"
+                --prefix PATH : "${pkgs.lib.makeBinPath (with pkgs; [ glow jq ])}"
 
               runHook postInstall
             '';
@@ -49,7 +45,6 @@
           xdgnj-bin = pkgs.stdenvNoCC.mkDerivation {
             name = "xdgnj-bin";
             version = "0.2.0.1-alpha";
-            description = "Pre-built binary of the xdgnj tool for creating and editing configuration files for xdg-ninja.";
             src = pkgs.fetchurl {
               url = "https://github.com/b3nj5m1n/xdg-ninja/releases/download/v0.2.0.1/xdgnj";
               sha256 = "y1BSqKQWbhCyg2sRgMsv8ivmylSUJj6XZ8o+/2oT5ns=";
@@ -59,6 +54,7 @@
               mkdir -p "$out/bin"
               install -Dm755 $src "$out/bin/xdgnj"
             '';
+            meta.description = "Pre-built binary of the xdgnj tool for creating and editing configuration files for xdg-ninja.";
           };
         };
         defaultPackage = packages.xdg-ninja;
